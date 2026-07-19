2f2450d1a5d481fa622fe7e8e76ca4b87f0b317a
tr100o <trkymflh79@gmail.com>
2026-07-19 04:03:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     log: '1528129119827791973',
     owners: ['910264482444480562'],
     prefix: '!',
-    token: 'MTUyODEyNjcwMzYyMzIwOTAzMA.GtUY9P.9yP29J9_BvFr7hk1NMf6CQvRYGAfweUW_x9rHI',
+    token: 'MTUyODEyNjcwMzYyMzIwOTAzMA.GJwK5O.VaGvAEGtE_YwGajUck_j8BWFP3ym2BMmxEWqbk',
     clientId: '1528126703623209030',
     clientSecret: 'AAsl9mF_K135CavHzMHLNwXJ-TnCQ4BS',
     redirectUri: 'https://discord-cloner-bot.vercel.app/callback',
