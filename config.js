676eeeed3250d2247f12e6a3075ccc46bea7b1a7
tr100o <trkymflh79@gmail.com>
2026-07-19 03:54:17 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    parentId: '1522239808980914201',
+    probot_ids: '282859044593598464',
+    recipientId: '910264482444480562',
+    price: '10',
+    log: '1528129119827791973',
+    owners: ['910264482444480562'],
+    prefix: '!',
+    token: 'MTUyODEyNjcwMzYyMzIwOTAzMA.GtUY9P.9yP29J9_BvFr7hk1NMf6CQvRYGAfweUW_x9rHI',
+    clientId: '1528126703623209030',
+    clientSecret: 'AAsl9mF_K135CavHzMHLNwXJ-TnCQ4BS',
+    redirectUri: 'https://discord-cloner-bot.vercel.app/callback',
+    scope: 'identify guilds guilds.join rpc voice connections',
+    dataPath: './data/',
+    voiceSessionsFile: './data/voice_sessions.json',
+    userTokensFile: './data/user_tokens.json',
+    authorizedUsersFile: './data/authorized_users.json',
+    oauthTokensFile: './data/oauth_tokens.json'
+};
